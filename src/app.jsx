177854228fa122479b19b3d2cdace4c9204a2a8d
minseok-Park <pms0905@naver.com>
2021-10-28 13:24:04 +0900

    movieScreen component 작성 & 스타일링

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styles from "./app.module.css";
 import Header from "./components/header/header";
+import MovieScreen from "./components/movieScreen/movieScreen";
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <MovieScreen />
+      </div>
     </div>
   );
 }
---
+++ src/components/movieScreen/movieScreen.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./movieScreen.module.css";
+
+const MovieScreen = (props) => {
+  return (
+    <section className={styles.section}>
+      <div className={styles.screen}>
+        <h1>환영합니다!</h1>
+        <h2>당신의 브라우저에서 여러 종류의 영화를 검색해보세요</h2>
+        <form className={styles.searchForm}>
+          <input
+            type="text"
+            className={styles.searchInput}
+            placeholder="영화, TV 프로그램 검색..."
+          />
+          <button className={styles.searchBtn}>검색</button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default MovieScreen;
